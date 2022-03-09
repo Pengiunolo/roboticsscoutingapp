aa49eaa5962708e76976d1209acde73b28039d4a
victor <therealpenguinolo@gmail.com>
2022-03-08 19:48:02 -0500

    added more to the match database

--- dbHandler.py
+++ dbHandler.py
@@ -5,4 +5,5 @@ def initDB():
     global conn
     conn.execute("create table if not exists Teams(TeamName text,TeamNum int)")
     conn.execute("create table if not exists Matchs(MatchNumber int,Compitions )")
-    conn.execute("create table if not exists Compitions(Name text,)")
+    conn.execute("create table if not exists Compitions(Name text, Date int)")
+    conn.execute("create table if not exists MatchScoring(MatchId int, TeamNum1 int, TeamNum2 int, Allince text,Team1_BoxToHub int, Team2_BoxToHub int, Team1_BoxToLevel int, Team2_boxToLevel int,Team1_Duck int, Team2_duck int, Team1_Park int, Team2_park int, Team1_ExtraCubes int, Team2_ExtraCubes int)")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from inputHandler import *
+from dbHandler import *
 
 root = tk.Tk()
 
@@ -8,3 +9,4 @@ title= tk.Label(master = root,text = 'Scouting application')
 title.grid(row=1,column=3)
 teamSwitcher=tk.Button(text='Switch team',command= lambda: teamSwitch(teamSwitcher),bg="red",activebackground="red");
 teamSwitcher.grid(row=1,column=1,padx=(10,50),pady=30);
+initDB()
